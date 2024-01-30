640e6a388db8f1f5e2d9f30b6b55876996453754
Puskass <cosovictarik711@gmail.com>
2024-01-30 17:20:34 +0100

    feat: submiti data in form

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
+
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
 app.get("/signup", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'signup.html'))
-})
+  res.sendFile(path.join(__dirname, "views", "signup.html"));
+});
 
-app.listen(5000)
+app.post("/signup", (req, res) => {
+  //
+  const { username, password } = req.body
+  const user = { 
+    username,
+    password
+  }
+  console.log(user);
+  res.redirect("/")
+});
+
+app.listen(5000);
