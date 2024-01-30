543c7661d407d5ec9f58ea0f7227668ddcc66b74
Puskass <cosovictarik711@gmail.com>
2024-01-30 20:28:15 +0100

    feat: dynamic content

--- index.js
+++ index.js
@@ -2,31 +2,35 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const p = path.join(__dirname, "data", "products.json");
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.get("/signup", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "signup.html"));
-});
-
-app.post("/signup", (req, res) => {
-  const { username, password } = req.body;
-
-  const usersDataPath = path.join(__dirname, "data", "users.json");
-
-  fs.readFile(usersDataPath, (err, usersAsJSON) => {
-    const users = JSON.parse(usersAsJSON);
-
-    const updatedUsers = [...users, { username, password }];
+  fs.readFile(p, (err, products) => {
+    res.render("index", {
+      pageTitle: "Web Shop",
+      products: JSON.parse(products),
+    });
   });
+});
 
-  fs.writeFile(usersDataPath, JSON.stringify(updatedUsers), () => {
-    res.redirect("/");
+app.get("/admin/add-product", (req, res) => {
+  res.render("add-product", { pageTitle: "Web Shop - Add New Product" });
+});
+
+app.post("/admin/add-product", (req, res) => {
+  const { title, price } = req.body;
+
+  fs.readFile(p, (err, products) => {
+    const updatedProducts = [{ title, price }, ...JSON.parse(products)];
+    fs.writeFile(p, JSON.stringify(updatedProducts), () => {
+      res.redirect("/");
+    });
   });
 });
 
