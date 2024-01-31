2bad87668d2cb8b8ce9dfb5e903f1cfc7d1a9059
Puskass <cosovictarik711@gmail.com>
2024-01-31 16:31:33 +0100

    feat: random id dynamic view product details

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { v4 } = require('uuid')
 
 const p = path.join(__dirname, "data", "products.json");
 const app = express();
@@ -10,11 +11,12 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => res.redirect("/products"))
+app.get("/products", (req, res) => {
   fs.readFile(p, (err, products) => {
     res.render("index", {
       pageTitle: "Web Shop",
-      products: JSON.parse(products),
+      products: JSON.parse(products), 
     });
   });
 });
@@ -25,9 +27,14 @@ app.get("/admin/add-product", (req, res) => {
 
 app.post("/admin/add-product", (req, res) => {
   const { title, price } = req.body;
+  const product = {
+    id: v4(),
+    title,
+    price
+  }
 
   fs.readFile(p, (err, products) => {
-    const updatedProducts = [{ title, price }, ...JSON.parse(products)];
+    const updatedProducts = [product, ...JSON.parse(products)];
     fs.writeFile(p, JSON.stringify(updatedProducts), () => {
       res.redirect("/");
     });
